5f91c479960829ec7b1e9a95aeeec4a45ef7bea8
nl <nunolopes98@hotmail.com>
2018-02-15 17:29:34 +0000

    Iteration #1 - Task #1

---
+++ LPOO Project/src/Game.java
@@ -0,0 +1,30 @@
+
+public class Game {
+
+	static char[][] map = {{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','H',' ',' ','I',' ','X',' ','G','X'},
+					{'X','X','X',' ','X','X','X',' ',' ','X'},
+					{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+					{'X','X','X',' ','X','X','X',' ',' ','X'},
+					{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+					{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+					{'X','X','X',' ','X','X','X','X',' ','X'},
+					{'X',' ','I',' ','I',' ','X','k',' ','X'},
+					{'X','X','X','X','X','X','X','X','X','X'}};
+	
+	public static void printMap() {
+		for(int i = 0; i<map.length; i++) {
+			System.out.print("|");
+			for(int j = 0; j< map[i].length; j++) {
+				System.out.print(map[i][j]+"|");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		printMap();
+
+	}
+	
+}
