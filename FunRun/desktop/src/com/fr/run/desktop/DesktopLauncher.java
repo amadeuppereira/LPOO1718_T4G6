85f0a2e6da82675ddbbd989350626a63bc961c1a
Masterflopes <up201605337@fe.up.pt>
2018-04-12 16:44:34 +0100

    FunRun

---
+++ FunRun/desktop/src/com/fr/run/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fr.run.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fr.run.RunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunGame(), config);
+	}
+}
